bbb986bcd975a7ff99543fca7eccc36027e02505
Merlici-Umut <umut.merlici@elev.cihcahul.md>
2021-03-16 14:11:15 +0200

    comit

---
+++ Practica/resources/views/welcome.blade.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="" href="resources/css/main.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <style>
+        body{
+            margin: 0 110px;
+
+        }
+        .container{
+
+            padding-bottom: 200px;
+        }
+        input{
+            border: 2px solid white;
+            width: 150px;
+            position: absolute;
+            top: 50%;
+            left: 33%;
+            display: inline-block;
+            background-color: transparent;
+            color: white;
+            padding: 5px 20px;
+            transition-duration: 0.4s;
+        }
+        .button1:hover{
+            background-color: white;
+            color: black;
+        }
+        .button2{
+            left: 50%;
+        }
+        .button2:hover{
+            background-color: white;
+            color: black;
+        }
+    </style>
+</head>
+<body style="">
+    <div class="container" style=" background-image: url('https://wallpapercave.com/wp/wp3525156.jpg'); background-repeat: no-repeat; background-size: 100% ">
+        <div class="row">
+            <h1 style="padding-top:150px; font-size: 60px; color: white; text-align: center">THIS IS SPECTRUM</h1>
+            <h5 style="padding-bottom: 20px; color: white; text-align: center">Responsive Retina Ready.Sketch Support</h5>
+            <div class="btn-group mb-2">
+                <input type="button" class="button1"  value="START NOW">
+                <input type="button" class="button2"   value="LEARN MORE">
+            </div>
+        </div>
+    </div>
+    <div class="container" style="padding-top: 100px">
+        <div class="row">
+            <h3 style="text-align: center">ABOUT US</h3>
+            <p style="text-align: center ;" >_________</p>
+        </div>
+    </div>
+</body>
+</html>
